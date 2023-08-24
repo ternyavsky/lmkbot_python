e5664f770a8aa0e7ca383d3a357afd9220e38ee4
ternyavky <ilya.ternyavsky@yandex.ru>
2023-08-24 17:51:23 +0300

    python

---
+++ main.py
@@ -0,0 +1,63 @@
+import logging
+import fitz
+import time
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from utils import get_shedule, get_color
+
+API_TOKEN = '5889044797:AAEdD4qXKzJE9KRuKAKKsv8-aabIM6qkLCQ'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(API_TOKEN, parse_mode="HTML")
+dp = Dispatcher(bot=bot)
+
+
+
+kb = [
+        [types.KeyboardButton(text='Замены'), 
+         types.KeyboardButton(text="Цвет недели")]
+    ]
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+    start = time.time()
+    await message.reply(f"Привет, {message.chat.first_name}! Это бот иcип, замены и цвет недели - здесь", reply_markup=keyboard)
+    print(time.time() - start)
+
+
+@dp.message_handler(content_types=['text'])
+async def color_week(message):
+    start = time.time()
+    if message.text == 'Цвет недели':
+        a = get_color()
+        await message.answer(a)
+
+    if message.text == 'Замены':
+        start = time.time()
+        print('work')
+        a = get_shedule()
+        doc = fitz.open("shedule.pdf")
+        media = types.MediaGroup()
+        for i in range(len(doc)):
+            page = doc.load_page(i)  # number of page
+            pix = page.get_pixmap()
+            output = "page"+str(i+1)+".png" # first create the output folder in the destination
+            pix.save(output)
+            if len(doc) - 1 == i:
+                media.attach_photo(photo=types.InputFile(output), caption=a)
+            else:
+                media.attach_photo(photo=types.InputFile(output))
+        end = print(time.time() - start)
+        await message.answer_media_group(media=media)
+    
+
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ utils.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import time
+
+url = 'http://lmk-lipetsk.ru/main_razdel/shedule/index.php'
+res = requests.get(url)
+soup = bs(res.content, 'html.parser')
+def get_shedule():
+    start = time.time()
+    tags =soup.find_all('a', target='_blank')
+    result = None
+    name = None
+    for i in tags:
+        if 'Расписание занятий' in i.text:
+            result = i['href']
+            name = i.text
+
+    sec_url = 'http://lmk-lipetsk.ru{}'.format(result)
+    get_pdf = requests.get(sec_url)
+    with open ('shedule.pdf', 'wb') as f:
+        f.write(get_pdf.content)
+    print(time.time() - start)
+    return name
+
+def get_color():
+    tags = soup.find_all('h3')
+    result = None
+    for i in tags:
+
+        if 'неделя' in i.text:
+            result = i.text 
+    return result
+
